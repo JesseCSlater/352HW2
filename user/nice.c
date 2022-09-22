38dfc672fb7f5912d8431dad5c2da8adb1ddc625
John Mindrup <mindrup.john@gmail.com>
2022-09-22 15:12:56 -0500

    2.4 Added nice.c

---
+++ user/nice.c
@@ -0,0 +1,31 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    //the nice value
+    int nicevalue;
+    if(argc < 2)
+    printf("Too few arguments expected nice N PROG [ARG] ...\n");
+    /**
+     * atoi should return 0 if given something that starts with a non-number
+     * check if its '-' and if so give it the string argument indexed one further
+     * if not give atoi the full string
+     * if the argument starts with numbers it will convert those as an int
+     * will default to zero if a non-number is given
+     */
+    if(argv[1][0] == '-')
+    {
+        nicevalue = atoi(argv[1]+1);
+    }
+    else
+    {
+        nicevalue = atoi(argv[1]);
+    }
+    nice(nicevalue);
+    if(argc < 3)
+    printf("Too few arguments expected nice N PROG [ARG] ...\n");
+    exec(argv[2], &argv[2]);
+    exit(0);
+}
